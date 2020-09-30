488f2e457c8f070d9b7d5d564c03d677d7ee57c4
Milton Jesús Vera Contreras <miljeveco@gmail.com>
2020-09-30 17:38:34 -0500

    package

---
+++ test/Test.java
@@ -0,0 +1,24 @@
+package test;
+
+
+/**
+ * Use los comentarios para explicar el objetivo de este programa Test .
+ *
+ * @author (Milton J. Vera Contreras miljeveco@gmail.com)
+ * @version 0.00000001
+ */
+public class Test
+{
+    //entradas
+
+   //salidas
+
+   //algoritmo
+    
+    void algoritmo( ){
+      
+      // Aqui ponga su algoritmo
+    
+    }//fin method
+    
+}//fin class Test
