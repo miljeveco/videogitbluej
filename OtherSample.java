71f48e78d4f119961879f65716702b87903096e8
Milton Jesús Vera Contreras <miljeveco@gmail.com>
2021-08-26 19:05:14 -0500

    Test git token

---
+++ OtherSample.java
@@ -0,0 +1,22 @@
+
+/**
+ * Use los comentarios para explicar el objetivo de este programa OtherSample .
+ *
+ * @author Ponga aqui su nombre y correo
+ * @version Ponga aquí la versión o fecha
+ */
+public class OtherSample
+{
+    //entradas
+
+    //salidas
+
+    //algoritmo
+    
+    void algoritmo( ){
+      
+      // Aqui ponga su algoritmo
+    
+    }//fin method
+    
+}//fin class OtherSample
